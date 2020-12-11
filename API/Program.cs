641999afd45e82870cf5984c3ce9f090c506f43c
PonRajKumar Rajendran <prkumarrajen@hotmail.com>
2020-12-11 16:54:14 +0530

    program cs change

--- API/Program.cs
+++ API/Program.cs
@@ -16,7 +16,7 @@ namespace API
 {
     public class Program
     {
-        public static async void Main(string[] args)
+        public static async Task Main(string[] args)
         {
             CreateHostBuilder(args).Build().Run();
             var host = CreateHostBuilder(args).Build();
